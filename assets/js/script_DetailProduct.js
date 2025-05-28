6de5f0df1f709dd706ffba7e1c33a3c4377038e5
tbtrann1912 <tranbaotrann04@gmail.com>
2025-05-28 23:47:36 +0700

    tran cao

--- assets/js/script_DetailProduct.js
+++ assets/js/script_DetailProduct.js
@@ -1,10 +1,16 @@
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   const carousels = document.querySelectorAll(".carousel-wrapper");
 
   //lấy id sản phẩm từ url
   const searchParams = new URLSearchParams(window.location.search);
   const id = searchParams.get("id");
+
+  //lấy thông tin sản phẩm từ api
+  const product = await fetch(`http://localhost:3000/products/${id}`);
+  const productData = await product.json();
+
+  const phoneNumber = document.querySelector(".phone-number");
+  phoneNumber.textContent = productData.phoneNumber;
 
   carousels.forEach(wrapper => {
     const bookList = wrapper.querySelector(".book-list");
