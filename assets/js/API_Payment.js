13ce787f77a952d5a518e0709360e77b8972f0ff
tbtrann1912 <tranbaotrann04@gmail.com>
2025-05-29 21:10:17 +0700

    Refactor order creation in API_Payment.js to structure order data more clearly; update payment.html for improved HTML formatting and readability.

--- assets/js/API_Payment.js
+++ assets/js/API_Payment.js
@@ -2,10 +2,22 @@ const API_BASE_URL = 'https://tiemsachnhaem-be-mu.vercel.app/api';
 
 const orderAPI = {
     createOrder: async (data) => {
+        const orderData = {
+            userId: data.userId,
+            products: data.items.map(item => ({
+                productId: item.bookId,
+                quantity: item.quantity,
+                price: item.price
+            })),
+            paymentMethod: data.paymentMethod,
+            shippingAddress: data.shippingAddress
+        }
+        // console.log(data);
+
         const response = await fetch(`${API_BASE_URL}/orders`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
+            body: JSON.stringify(orderData)
         });
         if (!response.ok) throw new Error('Failed to create order');
         return response.json();
